4aecf16ff2ee70803a39054e486fd11c3e6776d7
foysal90 <foysal1416@hotmail.com>
2023-03-08 23:43:19 -0500

    added exceptionHandling

---
+++ src/exceptionHandling/demo.java
@@ -0,0 +1,15 @@
+package exceptionHandling;
+
+public class demo {
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 6;
+        int z = x + y;
+
+        System.out.println(z);
+
+
+
+    }
+}
